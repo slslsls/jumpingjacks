d311b8d8171207754956859182d86193ea02e592
Spencer Scarpelli <spencerscarpelli@gmail.com>
2019-10-28 21:05:45 -0700

    finish getRandomScenario logic

--- src/utils/scenarioUtils.js
+++ src/utils/scenarioUtils.js
@@ -1,16 +1,29 @@
 const groups = require('../scenarios/groups');
 const preFlop = require('../scenarios/pre-flop');
+const stages = { // todo: add other stages when the json has been created
+  preFlop
+};
 
 function getRandomScenario(args) {
-  let action = args.action ? args.action : randomObjectKey(preFlop);
-  let position = args.position ? args.position : randomObjectKey(preFlop.Raise);
-  let leadUp = args.leadUp ? args.leadUp : randomObjectKey(preFlop.Raise.Early);
-  return 
+  let stage = args && args.stage ? args.stage : randomObjectKey(stages);
+  let stageObject = stages[stage];
+  let action = args && args.action ? args.action : randomObjectKey(stageObject);
+  let position = args && args.position ? args.position : randomObjectKey(stageObject[action]);
+  let leadUp = args && args.leadUp ? args.leadUp : randomObjectKey(stageObject[action][position]);
+  let potentialGroups = stageObject[action][position][leadUp];
+  let selectedGroup = randomArrayElement(potentialGroups);
+  let cards = randomArrayElement(groups[selectedGroup]);
+  return { stage, action, position, leadUp, selectedGroup, cards };
 }
 
 function randomObjectKey(obj) {
   let idx = randomIntFromInterval(0, Object.keys(obj).length - 1);
   return Object.keys(obj)[idx];
+}
+
+function randomArrayElement(arr) {
+  let idx = randomIntFromInterval(0, arr.length - 1);
+  return arr[idx];
 }
 
 function randomIntFromInterval(min, max) {

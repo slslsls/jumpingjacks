f472b5d15dfeddaec872b9c0a8a8900feb11962f
Spencer Scarpelli <spencerscarpelli@gmail.com>
2019-11-16 09:50:43 -0800

    ease-in home page load

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,22 @@ import { Link } from 'react-router-dom';
 import './Home.css';
 
 class Home extends React.Component {
+  state = {
+    display: false
+  };
+
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        display: true,
+      });
+    }, 300);
+  }
+
   render() {
     return (
       <div>
-        <div className="home-background">
+        <div className={`${this.state.display ? '' : 'invisible'} home-background`}>
           <div className="home-frame">
             <div className="nav-button">
               <Link to="/blackjack">Blackjack</Link>

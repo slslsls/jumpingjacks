d0113c0550bffdaf7d038d67c4d24948c980bc87
Spencer Scarpelli <spencerscarpelli@gmail.com>
2019-11-19 22:04:58 -0800

    add english and mandarin language buttons

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,16 @@ import { Link } from 'react-router-dom';
 import Translations from '../utils/common/translations';
 import './Home.css';
 
+const languages = [
+  Translations.english_label,
+  Translations.mandarin_label
+];
+
 class Home extends React.Component {
   state = {
     display: false,
     text: Translations.mandarin.home
   };
-
 
   componentDidMount = () => {
     setTimeout(() => {
@@ -23,7 +27,15 @@ class Home extends React.Component {
       <div>
         <div className={`${this.state.display ? '' : 'invisible'} home-background`}>
           <div className="home-frame">
-            <div className="language-window"></div>
+            <div className="language-window">
+              {languages.map((l, idx ) =>
+                <div
+                  key={idx}
+                  className="language"
+                >
+                  {l}
+                </div>)}
+            </div>
             <Link to="/blackjack">
               <div className="nav-button">{this.state.text.practice_blackjack}</div>
             </Link>
